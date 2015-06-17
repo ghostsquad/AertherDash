cbd0daa554c6c4c4c6bc05e04b4b7ff5312e8232
Weston McNamee <ghost.squadron@gmail.com>
2015-06-17 00:01:49 -0700

    starting with polymer

---
+++ AetherDash/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace AetherDash
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
